bf14f149d94a5407140c8bc386380a04f9346a93
tony <chcjswo@gmail.com>
2021-07-19 00:39:22 +0900

    Global Filter 적용

---
+++ src/main/java/com/mocadev/apigatewayservice/filter/GlobalFilter.java
@@ -0,0 +1,54 @@
+package com.mocadev.apigatewayservice.filter;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author chcjswo
+ * @version 1.0.0
+ * @blog https://mocadev.tistory.com
+ * @github https://github.com/chcjswo
+ * @since 2021-07-14
+ **/
+@Slf4j
+@Component
+public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
+
+	public GlobalFilter() {
+		super(Config.class);
+	}
+
+	@Data
+	public static class Config {
+		private String baseMessage;
+		private boolean preLogger;
+		private boolean postLogger;
+	}
+
+	@Override
+	public GatewayFilter apply(Config config) {
+		return (exchange, chain) -> {
+			ServerHttpRequest request = exchange.getRequest();
+			ServerHttpResponse response = exchange.getResponse();
+
+			log.info("Global filter baseMessage: {}", config.getBaseMessage());
+
+			if (config.isPreLogger()) {
+				log.info("Global Filter start: request id -> {}", request.getId());
+			}
+
+			return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+				if (config.isPostLogger()) {
+					log.info("Global POST filter: response code -> {}", response.getStatusCode());
+				}
+			}));
+		};
+	}
+
+}
