cd898bd75c77b7f6cbdd24d3b10e7d4c012d126e
tony <chcjswo@gmail.com>
2021-07-19 00:55:27 +0900

    Logging Filter 적용

--- src/main/java/com/mocadev/apigatewayservice/filter/LoggingFilter.java
+++ src/main/java/com/mocadev/apigatewayservice/filter/LoggingFilter.java
@@ -3,7 +3,9 @@ package com.mocadev.apigatewayservice.filter;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
@@ -18,9 +20,9 @@ import reactor.core.publisher.Mono;
  **/
 @Slf4j
 @Component
-public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
+public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Config> {
 
-	public GlobalFilter() {
+	public LoggingFilter() {
 		super(Config.class);
 	}
 
@@ -33,22 +35,22 @@ public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Conf
 
 	@Override
 	public GatewayFilter apply(Config config) {
-		return (exchange, chain) -> {
+		return new OrderedGatewayFilter((exchange, chain) -> {
 			ServerHttpRequest request = exchange.getRequest();
 			ServerHttpResponse response = exchange.getResponse();
 
-			log.info("Global filter baseMessage: {}", config.getBaseMessage());
+			log.info("Logging filter baseMessage: {}", config.getBaseMessage());
 
 			if (config.isPreLogger()) {
-				log.info("Global PRE Filter start: request id -> {}", request.getId());
+				log.info("Logging PRE Filter start: request id -> {}", request.getId());
 			}
 
 			return chain.filter(exchange).then(Mono.fromRunnable(() -> {
 				if (config.isPostLogger()) {
-					log.info("Global POST filter: response code -> {}", response.getStatusCode());
+					log.info("Logging POST filter: response code -> {}", response.getStatusCode());
 				}
 			}));
-		};
+		}, Ordered.LOWEST_PRECEDENCE);
 	}
 
 }
